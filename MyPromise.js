e04a4720ac63cfc42b2e476a63ef0c04e62c989e
timur <asadullintimur04@gmail.com>
2023-10-20 17:16:26 +0300

    added a promise chain

--- MyPromise.js
+++ MyPromise.js
@@ -1,20 +1,37 @@
 export default class MyPromise {
+    state = null;
+    result = null;
+
     callbacks = [];
+    callbacksPromise = new Map();
 
     constructor(func) {
-        func(this._getResolveWithContext(), this._getRejectWithContext())
+        if (func) func(this._getResolveWithContext(), this._getRejectWithContext())
+    }
+
+    _addCallbackPromise(callback) {
+        const callbackPromise = new MyPromise();
+        this.callbacksPromise.set(callback, callbackPromise)
+
+        return callbackPromise;
     }
 
     then(callback) {
         this.callbacks.push({type: 'then', callback});
+
+        return this._addCallbackPromise(callback);
     }
 
     catch(callback) {
         this.callbacks.push({type: 'catch', callback});
+
+        return this._addCallbackPromise(callback);
     }
 
     finally(callback) {
         this.callbacks.push({type: 'finally', callback});
+
+        return this._addCallbackPromise(callback);
     }
 
     _getResolveWithContext() {
@@ -25,29 +42,79 @@ export default class MyPromise {
         return this._reject.bind(this);
     }
 
-    _getCallbacksByType(type) {
+    _getCallbacksByState() {
+        const state = this.state;
+
+        if (!state) return;
+
+        const type = state === 'resolved' ? 'then' : 'catch'
+
         return this.callbacks.filter(callback => callback.type === type || callback.type === 'finally')
     }
-    _executeCallbacks(callbacks, arg) {
+
+    _executeCallbacks() {
+        const callbacks = this._getCallbacksByState();
+
         callbacks.forEach(({type: callbackType, callback}) => {
             if (callbackType === 'finally') {
-                callback();
+                this._executeFinallyCallback(callback)
 
                 return;
             }
 
-            callback(arg)
+            this._executeThenCatchCallback(callback);
         })
     }
 
-    _resolve(arg) {
-        const callbacks = this._getCallbacksByType('then');
+    _passCurrentPromiseForward(callbackPromise) {
+        if (this.state === 'resolved') {
+            callbackPromise._resolve(this.result);
 
-        this._executeCallbacks(callbacks, arg)
+            return;
+        }
+
+        callbackPromise._reject(this.result);
     }
-    _reject(arg) {
-        const callbacks = this._getCallbacksByType('catch');
 
-        this._executeCallbacks(callbacks, arg)
+    _executeFinallyCallback(callback) {
+        const result = callback(),
+            callbackPromise = this.callbacksPromise.get(callback);
+
+        if (result instanceof MyPromise) {
+            result.then(() => callbackPromise._resolve(this.result));
+            result.catch(result => callbackPromise._reject(result));
+
+            return;
+        }
+
+        this._passCurrentPromiseForward(callbackPromise);
+    }
+
+    _executeThenCatchCallback(callback) {
+        const result = callback(this.result),
+            callbackPromise = this.callbacksPromise.get(callback);
+
+        if (result instanceof MyPromise) {
+            result.then(result => callbackPromise._resolve(result));
+            result.catch(result => callbackPromise._reject(result));
+
+            return;
+        }
+
+        callbackPromise._resolve(result);
+    }
+
+    _resolve(result) {
+        this.state = 'resolved';
+        this.result = result;
+
+        this._executeCallbacks()
+    }
+
+    _reject(result) {
+        this.state = 'rejected';
+        this.result = result;
+
+        this._executeCallbacks()
     }
 }
--- script.js
+++ script.js
@@ -2,21 +2,29 @@ import MyPromise from "./MyPromise.js";
 
 const myPromise = new MyPromise((resolve, reject) => {
     setTimeout(() => {
-        resolve('val')
+        resolve(2)
     }, 1000);
     // setTimeout(() => {
     //     reject(new Error('rejected'))
     // }, 1000);
 });
 
-myPromise.finally(result => {
-    console.log('myPromise finally handler', result)
-})
+const test =
+myPromise
+    .finally(() => {
+        console.log('finally handler')
 
-myPromise.then(result => {
-    console.log('myPromise resolve handler', result)
-})
+        return new MyPromise((resolve, reject) => setTimeout(() => reject(100), 1000));
+    })
+    .catch(result => {
+        console.log('then handler 1')
+        return new MyPromise((resolve, reject) => setTimeout(() => reject(result * 2), 500))
+    })
+    .catch(result => {
+        console.log('catch handler 2')
+        return new MyPromise(resolve => setTimeout(() => resolve(result * 2), 500))
+    })
+    .then(result => {
+        console.log(result)
+    })
 
-myPromise.catch(error => {
-    console.log('myPromise reject handler', error)
-})
